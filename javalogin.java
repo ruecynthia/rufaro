904f5ee760312e607cd8fe7ee35763d324263423
ruecynthia <chiyangwarufaro@gmail.com>
2016-05-03 12:10:59 -0800

    the first commit

---
+++ javalogin.java
@@ -0,0 +1,12 @@
+> - y (original data)
+ * <BR> - gt (Trend)
+  * <BR> - st (seasonal component)
+   * <BR> - rt (Rest)
+    * <BR> - tb (trend Adjusted)
+     * <BR> - sb (seasonally adjusted)
+      * <BR> - fb (Adjusted error)
+       
+        
+	mean absolute error (MAE) is a quantity used to measure how close forecasts or predictions are to the eventual outcomes.
+	mean squared error (MSE) or mean squared deviation (MSD) of an estimator measures the average of the squares of the errors or deviations,
+	
